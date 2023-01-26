19f294549eafaa163549381b3fc9c979514a41b6
kamlesh-67 <kamleshprajapat858@gmail.com>
2023-01-26 09:50:49 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// --------------------Pre Loader--------------
+let preloader = document.getElementById("preloader");
+
+window.addEventListener('load',()=>{
+    preloader.style.display="none"
+})
